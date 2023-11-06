069a2a07c4cba3dadc4adc5477ebb76bfdc32eae
sicilo <mejiamonsalve.camilo@gmail.com>
2023-11-06 11:28:15 -0500

    feat: adding loader spinner

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# 04CountryApp
+# Countries App
 
 This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 16.2.7.
 
--- src/app/countries/services/countries.service.ts
+++ src/app/countries/services/countries.service.ts
@@ -1,6 +1,6 @@
 import { HttpClient, HttpErrorResponse } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Observable, catchError, map, of } from 'rxjs';
+import { Observable, catchError, delay, map, of } from 'rxjs';
 import { ICountry } from '../interfaces/countries.interfaces';
 
 @Injectable({
@@ -11,6 +11,15 @@ export class CountriesService {
   private apiUrl: string = 'https://restcountries.com/v3.1';
 
   constructor(private http: HttpClient) { }
+
+  private getCountriesRequest(url: string): Observable<Array<ICountry>> {
+    return this.http
+      .get<Array<ICountry>>(url)
+      .pipe(
+        catchError(() => of([])),
+        delay(2000)
+      );
+  }
 
   searchCountryByAlphaCode(code: string): Observable<ICountry | null> {
     let url = `${this.apiUrl}/alpha/${code}`;
@@ -28,34 +37,16 @@ export class CountriesService {
 
   searchCapital(term: string): Observable<Array<ICountry>> {
     let url = `${this.apiUrl}/capital/${term}`;
-    return this.http
-      .get<Array<ICountry>>(url)
-      .pipe(
-        catchError((error: HttpErrorResponse) => {
-          return of([]);
-        })
-      );
+    return this.getCountriesRequest(url);
   }
 
   searchCountry(term: string): Observable<Array<ICountry>> {
     let url = `${this.apiUrl}/name/${term}`;
-    return this.http
-      .get<Array<ICountry>>(url)
-      .pipe(
-        catchError((error: HttpErrorResponse) => {
-          return of([]);
-        })
-      );
+    return this.getCountriesRequest(url);
   }
 
   searchRegion(term: string): Observable<Array<ICountry>> {
     let url = `${this.apiUrl}/region/${term}`;
-    return this.http
-      .get<Array<ICountry>>(url)
-      .pipe(
-        catchError((error: HttpErrorResponse) => {
-          return of([]);
-        })
-      );
+    return this.getCountriesRequest(url);
   }
 }
